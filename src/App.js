9df9b0520320c407ec92580135d4c29d51a17451
fahimekhandan <fahimekhandan@gmail.com>
2023-11-15 22:42:26 +0330

    feat: add menu toggle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const side =
+    "w-96 bg-white h-screen  transform transition-all fixed duration-7000 text-white flex justify-center p-2 left-full";
+  const activeSide = " -translate-x-[390px]";
+
   return (
-    <div className="App">
-     
+    <div className="flex w-full h-screen">
+      <div className="bg-gray-700 grow p-10">
+        <button
+          className="bg-gray-200 p-4 text-primary-text"
+          onClick={() => setIsMenuOpen(true)}
+        >
+          Open The Menu
+        </button>
+      </div>
+
+      <div className={`${side} ${isMenuOpen ? activeSide : ""}`}></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "white": "#ffffff",
+      "primery-text": "#474E55",
+      "disabled-text": "#7B93AF",
+      "anhancer-text": "#007980",
+      "gray-100": " #F1F1F1",
+      "gray-200": "#D9D9D9",
+      "gray-700": "#2F2F2F",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
