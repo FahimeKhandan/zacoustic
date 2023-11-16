416b40345c4ab201df162cb7621772c1dd5a9566
fahimekhandan <fahimekhandan@gmail.com>
2023-11-16 11:16:32 +0330

    feat: add some colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      "white": "#ffffff",
-      "primary": "#474E55",
-      "disabled": "#7B93AF",
-      "anhancer": "#007980",
+      white: "#ffffff",
+      primary: "#474E55",
+      secandary: "#425B76",
+      disabled: "#7B93AF",
+      anhancer: "#007980",
       "gray-100": " #F1F1F1",
       "gray-200": "#D9D9D9",
       "gray-700": "#2F2F2F",
